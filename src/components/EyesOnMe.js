ac85a5d1a633d66357827369bd4b962630772aab
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-20 11:04:41 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+class EyesOnMe extends Component {
+  handleBlur = () => {
+    console.log("Good!");
+  };
+  handleFocus = () => {
+    console.log("Hey! Eyes on me!");
+  };
+  render() {
+    return (
+      <button onBlur={this.handleBlur} onFocus={this.handleFocus}></button>
+    );
+  }
+}
+
+export default EyesOnMe;
